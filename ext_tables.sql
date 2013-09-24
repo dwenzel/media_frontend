144bdf71e5519fe49e4224e663b4c359cf313fbc
Dirk Wenzel <der-wenzel@gmx.de>
2013-09-24 23:42:35 +0200

    field hidden added

--- ext_tables.sql
+++ ext_tables.sql
@@ -12,7 +12,7 @@ CREATE TABLE sys_file (
 	duration varchar(255) DEFAULT '' NOT NULL,
 	download_name varchar(255) DEFAULT '' NOT NULL,
 	frontend_user int(11) unsigned DEFAULT '0',
-
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
 );
 
 #
