9bd65803261e5fceec930f2b23a6bf099d325943
Dirk Wenzel <der-wenzel@gmx.de>
2013-09-25 15:04:33 +0200

    updated in extension builder

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,12 +5,11 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	'Webfox.' . $_EXTKEY,
-	'Media',
+	'Collection',
 	array(
-	//	'Asset' => 'list, show, new, create, edit, update, delete, upload, download',
-		'Asset' => 'list, show',
-	//	'FileCollection' => 'list, show, new, create, edit, update, delete',
-		'FileCollection' => 'list, show',
+		'Asset' => 'list, show, new, create, edit, update, delete, upload, download',
+		'FileCollection' => 'list, show, new, create, edit, update, delete',
+		
 	),
 	// non-cacheable actions
 	array(
@@ -20,4 +19,21 @@ if (!defined('TYPO3_MODE')) {
 	)
 );
 
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+	'Webfox.' . $_EXTKEY,
+	'Asset',
+	array(
+		'Asset' => 'list, show, new, create, edit, update, delete, upload, download',
+		'FileCollection' => 'list, show, new, create, edit, update, delete',
+		
+	),
+	// non-cacheable actions
+	array(
+		'Asset' => 'create, update, delete, ',
+		'FileCollection' => 'create, update, delete',
+		
+	)
+);
+
+## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
 ?>
