94c02b1c42c892e5f2a575eae18e3317e3bc11ce
Dirk Wenzel <der-wenzel@gmx.de>
2013-10-24 16:33:02 +0200

    FileCollection actions for searchForm and searchResult added

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,14 +8,14 @@ if (!defined('TYPO3_MODE')) {
 	'Media',
 	array(
 		'Asset' => 'list, show, new, create, edit, update',
-		'FileCollection' => 'list, show, new, create, edit, update, delete, newAsset, createAsset, listFeUser',
+		'FileCollection' => 'list, show, new, create, edit, update, delete, newAsset, createAsset, listFeUser, searchForm, searchResult',
 		
 	),
 	// non-cacheable actions
 	array(
 		'Asset' => 'create, update, delete, ',
 		'FileCollection' => 'create, update, delete, newAsset,
-		createAsset',
+		createAsset, searchForm, searchResult',
 		
 	)
 );
